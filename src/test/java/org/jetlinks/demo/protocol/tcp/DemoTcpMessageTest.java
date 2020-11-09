08798f79b6687e07753d093bc7a3a893e99e1cfb
wangzheng <540964791@qq.com>
2020-11-09 14:43:39 +0800

    优化

--- src/test/java/org/jetlinks/demo/protocol/tcp/DemoTcpMessageTest.java
+++ src/test/java/org/jetlinks/demo/protocol/tcp/DemoTcpMessageTest.java
@@ -6,17 +6,19 @@ import org.apache.commons.codec.binary.Hex;
 import org.jetlinks.core.message.codec.EncodedMessage;
 import org.jetlinks.core.message.property.ReadPropertyMessage;
 import org.jetlinks.demo.protocol.tcp.message.AuthRequest;
+import org.jetlinks.demo.protocol.tcp.message.FireAlarm;
 import org.jetlinks.demo.protocol.tcp.message.ReadProperty;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
+import java.util.concurrent.ThreadLocalRandom;
 
 class DemoTcpMessageTest {
 
     @Test
     void test() {
-        DemoTcpMessage message = DemoTcpMessage.of(MessageType.AUTH_REQ, AuthRequest.of(12345, "admin"));
+        DemoTcpMessage message = DemoTcpMessage.of(MessageType.AUTH_REQ, AuthRequest.of(1000, "admin"));
 
         byte[] data = message.toBytes();
         System.out.println(Hex.encodeHexString(data));
@@ -36,7 +38,7 @@ class DemoTcpMessageTest {
         DemoTcpMessageCodec demoTcpMessageCodec = new DemoTcpMessageCodec(null);
         ReadPropertyMessage readPropertyMessage = new ReadPropertyMessage();
         readPropertyMessage.setCode("10001");
-        readPropertyMessage.setDeviceId("111111");
+        readPropertyMessage.setDeviceId("1000");
         readPropertyMessage.setMessageId("test");
         readPropertyMessage.setTimestamp(LocalDateTime.now().getNano());
         DemoTcpMessage of = DemoTcpMessage.of(MessageType.READ_PROPERTY, ReadProperty.of(readPropertyMessage));
@@ -45,7 +47,28 @@ class DemoTcpMessageTest {
         byte[] payload = ByteBufUtil.getBytes(byteBuf, 0, byteBuf.readableBytes(), false);
         DemoTcpMessage message = DemoTcpMessage.of(payload);
         System.out.println(message.getType().getText());
+        System.out.println(Hex.encodeHexString(payload));
         ReadProperty data = (ReadProperty) message.getData();
         System.out.println(data.getReadPropertyMessage().toString());
     }
+
+    @Test
+    void encodeEvent() {
+        DemoTcpMessage demoTcpMessage = DemoTcpMessage.of(MessageType.FIRE_ALARM,
+                FireAlarm.builder()
+                        .point(ThreadLocalRandom.current().nextInt())
+                        .lat(36.5F)
+                        .lnt(122.3F)
+                        .deviceId(1000)
+                        .bName("a")
+                        .build());
+        byte[] data = new byte[500*1024];
+        System.out.println(demoTcpMessage);
+        System.out.println(Hex.encodeHexString(data));
+        //061400000000000000000003e842f43e7742cc77cf1bd9071c
+        //\06\14\00\00\00\00\00\00\00\00\00\03\e8B\f4>wB\ccw\cf\nt\c6\1a \06\14\00\00\00\00\00\00\00\00\00\03\e8B\f4>wB\ccw\cf\1b\d9\07\1c
+        //\06\14\00\00\00\00\00\00\00\00\00\03\e8B\f4>wB\ccw\cf\ba\f9#\aa
+    }
+
+
 }
